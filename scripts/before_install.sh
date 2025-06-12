34c8f370a1bdada3fbc98e56a9878f5080daacaa
atishthakur25 <atish.thakur@apnisec.com>
2025-06-12 13:10:28 +0530

    dev2

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 cd /home/ubuntu/next-js-app
 pm2 stop all || true
+#!/bin/bash
+
+# Stop running processes
+pm2 stop all || true
+
+# Remove existing app files to avoid conflict
+rm -rf /home/ubuntu/next-js-app/*
---
+++ scripts/clean.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "Cleaning up /home/ubuntu/next-js-app"
+rm -rf /home/ubuntu/next-js-app/*
