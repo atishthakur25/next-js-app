159c3f13a4df0939fef838ac5c1e5ec6583c3f2f
Atish Thakur <atish.kumar@apnisec.com>
2025-06-12 17:56:03 +0530

    title...

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         />
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="tracking-[-.01em]">
-            apnisec
+            apnisec.com
           </li>
         </ol>
     
