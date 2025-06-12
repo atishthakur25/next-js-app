d4f56ba38d16f5a736755174b7eb13580ca4371e
atishthakur25 <atish.thakur@apnisec.com>
2025-06-12 15:04:26 +0530

    dev4.

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 cd /home/ubuntu/next-js-app
-npm install
-npm run build
+if [ -d "node_modules" ]; then
+  echo "Dependencies already installed"
+else
+  echo "Installing dependencies..."
+  npm install
+fi
